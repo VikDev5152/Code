a217e2de0d744823c2843fec6bcc907ea35a4610
VikDev5152 <vikasdawar12.vd@gmail.com>
2026-01-13 12:08:44 +0000

    arrow function

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,39 @@
+//const user = {
+     //username: "vikas",
+     //price: 999,
+
+     //welcomeMessage: function(){
+       // console.log(`${this.username}, welcome to website`);
+        //console.log(this);
+     //}
+//}
+//user.welcomeMessage()
+//console.log(this);
+
+//function chaii(){
+  //  console.log(this);
+//}
+//chaii()
+
+//arrowfunction
+ //const chai =function (){
+   // let username = "hitesh"
+    //console.log(this.username);
+ //}
+
+ //const chai = () => { 
+    //let username = "hitesh"
+   //console.log(this);
+ //}
+ //chaii()
+
+
+ //const addTwo = (num1,num2) =>{
+   // return num1 + num2
+ //}
+
+ //console.log(addTwo(3,4))
+
+ //impicite return
+ const addTwo= (num1 , num2) => num1 + num2
+ console.log(addTwo(3,4))
